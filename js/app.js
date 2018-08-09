3d5357986d9d38d7defe5b3b7e91946ffec985e9
MaximusSirilius <40532864+siraj-mohammed@users.noreply.github.com>
2018-08-08 20:25:32 -0400

    should be working

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 //https://api.nytimes.com/svc/search/v2/articlesearch.json?q=$search&begin_date=$bDate&end_date=$eDate&limit=$responses&sort=newest&api_key=950ba3779f474d49aa1a605a55a6e151
 
 var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?sort=newest";
-var tSearch = $("#search").val();
-var bDate = $("#bDate").val();
-var eDate = $("#eDate").val();
+var tSearch = $("#searchterm").val();
+var bDate = $("#startyear").val();
+var eDate = $("#endyear").val();
 var apikey = "&api_key=950ba3779f474d49aa1a605a55a6e151";
-var nResponse = $("number").val();
+var nResponse = $("#NumberofSearch").val();
 
 if (tSearch !== ""){
 	queryURL += "&q=" + tSearch;
@@ -35,9 +35,13 @@ method: "GET"
 	results = response.docs;
 	
 	for(var i = 0; i < nResponse ; i++){
-	var newDiv = $("<div>");
-	var newNumber = $("<span>").addClass(".button");
-	newP.text(i+1);
-	
+		var newDiv = $("<div>");
+		var newNumber = $("<span>").addClass("button").text(i+1);
+		var newTitle = $("<span>").addClass("title").text(results.docs[i].headline.main);
+		var newP = $("<p>").text(results.docs[i].byline.original);
+		newDiv.append(newNumber);
+		newDiv.append(newTitle);
+		newDiv.append(newP);
+		$("#result").append(newDiv);	
 }
 });
